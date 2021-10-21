0fc2f532f5820c55e2cbb059c7e84f4ebabe65e8
pastweb <domenico.pasto@gmail.com>
2021-10-21 18:57:13 +0100

    cleanup exports

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,5 +1,4 @@
 export { fireWindowEvent } from './fireWindowEvent';
 export { setUserAgent } from './setUserAgent';
 export { windowResize } from './windowResize';
-export { MatchMedia } from './MatchMedia';
-export type { MediaQueryListener } from './MatchMedia';
+export { MatchMedia, MediaQueryListener } from './MatchMedia';
