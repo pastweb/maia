4e20ab59468de77dd415f1903505dba9820e9c9b
pastweb <domenico.pasto@gmail.com>
2021-10-30 12:41:01 +0100

    cleanup

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,6 +2,5 @@ export { AppAdapter } from './AppAdapter';
 export { useDevice, DevicesConfig, IsDevicesResult } from './useDevice';
 export { useDidMount } from './useDidMount';
 export { useForceUpdate } from './useForceUpdate';
-export { useMergeState } from './useMergeState';
 export { useWillMount } from './useWillMount';
 export { useWillUnmount } from './useWillUnmount';
