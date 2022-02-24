f764bde5bf4851df5860ba6dfb24679a91c6f860
pastweb <domenico.pasto@gmail.com>
2022-02-24 21:00:01 +0000

    removed annoying rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
   overrides: [
     {
